0e746e39a0ed88c9393103b0203a5d8366077ec2
Arindam <iamari.cse@gmail.com>
2017-08-29 12:01:55 +0530

    Navigation Drawer added

--- Kiddypi/Kiddypi/View/MenuPageMenuItem.cs
+++ Kiddypi/Kiddypi/View/MenuPageMenuItem.cs
@@ -15,6 +15,7 @@ namespace Kiddypi.View
         }
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Icon { get; set; }
 
         public Type TargetType { get; set; }
     }
